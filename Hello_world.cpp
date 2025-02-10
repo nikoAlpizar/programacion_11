6cfc16d8f670e30608ec903bb621141bc7f915b8
Nikolas Alpizar <nikonk2008@gmail.com>
2025-02-09 20:29:03 -0600

    Codigo de la tarea001
    Signed-off-by: Nikolas Alpizar <nikonk2008@gmail.com>

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,1 +1,18 @@
-int var_1 = 0; 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, suma, multiplicacion;
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+    suma = num1 + num2;
+    multiplicacion = num1 * num2;
+    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << endl;
+    cout << "La multiplicación de " << num1 << " y " << num2 << " es: " << multiplicacion << endl;
+
+    return 0;
+}
