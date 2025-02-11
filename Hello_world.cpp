535dce86dec94718d859c347be0aa8d4e3914f69
Nikolas Alpizar <nikonk2008@gmail.com>
2025-02-10 21:21:44 -0600

    jhchfcghcgcgcgcg
    Signed-off-by: Nikolas Alpizar <nikonk2008@gmail.com>

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main() {
     double num1, num2, suma, multiplicacion;
-    cout << "Ingrese el primer número: ";
+    cout << "Ingrese el primer numero: ";
     cin >> num1;
-    cout << "Ingrese el segundo número: ";
+    cout << "Ingrese el segundo numero: ";
     cin >> num2;
     suma = num1 + num2;
     multiplicacion = num1 * num2;
     cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << endl;
-    cout << "La multiplicación de " << num1 << " y " << num2 << " es: " << multiplicacion << endl;
+    cout << "La multiplicacion de " << num1 << " y " << num2 << " es: " << multiplicacion << endl;
 
     return 0;
 }
